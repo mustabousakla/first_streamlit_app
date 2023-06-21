9f59865defe94602e07a7d448e688d7a2a04b5d2
Mustapha <107427472+mustabousakla@users.noreply.github.com>
2023-06-21 13:07:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,8 @@ import streamlit
 
 streamlit.title("My Parents New Healthu Dinner")
 
+streamlit.subtitle("Breakfast Menu")
+
 streamlit.text("Kale, Spinach & Rocket Smoothie")
+streamlit.text("Omega 3 & Blueberry Oatmeal")
+streamlit.text("Hard-boiled Free-Range Egg")
